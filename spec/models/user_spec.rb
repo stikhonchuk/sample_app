0228501b188b52fe7efc31269d7ba627da23958f
Sergey <stikhonchuk@mail.ru>
2013-01-25 00:16:07 +0400

    Add user microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@
 #  created_at      :datetime        not null
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 require 'spec_helper'
@@ -25,6 +27,7 @@ describe User do
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:admin) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:microposts) }
 	it { should be_valid }
 	it { should_not be_admin }
 	describe "with admin attribute set to 'true'" do
@@ -97,4 +100,33 @@ describe User do
 		before { @user.save }
 		its(:remember_token) { should_not be_blank }
 	end
+	describe "micropost associations" do
+		before { @user.save }
+		let!(:older_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+		end
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+		end
+		it "should have the right microposts in right order" do
+			@user.microposts.should == [newer_micropost, older_micropost]
+		end
+		it "should destroy associated microposts" do
+			microposts = @user.microposts.dup
+			@user.destroy
+			microposts.should_not be_empty
+			microposts.each do |micropost|
+				Micropost.find_by_id(micropost.id).should be_nil
+			end
+		end
+		describe "status" do
+			let(:unfollowed_post) do
+				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+			end
+			its(:feed) { should include(newer_micropost) }
+			its(:feed) { should include(older_micropost) }
+			its(:feed) { should_not include(unfollowed_post) }
+		end
+	end
+
 end
